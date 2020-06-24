54f828e787fcb86e8da267f2bc40f726484d0aaa
IM-TECHNO <58085092+IM-TECHNO@users.noreply.github.com>
2020-06-24 04:48:47 -0400

    Add files via upload

---
+++ VOR.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+figlet MADE BY TECHNO
+echo -e "\e[44mChecking dependencies...\e[0m"
+echo ""
+sleep 3s
+if command -v figlet >/dev/null 2>&1 ; then
+    echo "Dependencies are installed "
+echo""   
+else
+    echo -e "\e[31mrun install.sh to install dependencies\e[0m"
+fi
+sleep 2s
+clear
+for (( ; ; ))
+do
+echo -e "\e[44m\e[31m\e[1m Volume of the Rectangle\e[0m" 
+echo ""
+echo " Enter the length :"
+read l
+echo " Enter Your breadth :"
+read b
+echo " Enter the Height :"
+read h
+echo ""
+echo -e "\e[31m\e[42m\e[1m THE ANSWER IS\e[0m" = $(( $h * $b * $l))
+echo ""
+echo -e "\e[31mPRESS [CTRL+C] TO STOP\e[0m"
+echo""
+echo""
+done 
+
---
+++ install.sh
@@ -0,0 +1,6 @@
+apt-get install figlet
+echo ""
+echo -e "\e[41mDependencies has been installed. Please Wait a few sec\e[0m"
+sleep 4s
+echo ""
+echo -e "\e[42mThe installation is now complete !. Type bash VOR.sh to start\e[0m"
